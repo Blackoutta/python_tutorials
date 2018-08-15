f272e884ab50a7ea51dfe5d56516f2b824b05742
Yang Hu <hyytez@gmail.com>
2018-08-15 13:27:03 +0800

    update 2018.8.15
    Signed-off-by: Yang Hu <hyytez@gmail.com>

---
+++ treehouse/object/master_class.py
@@ -0,0 +1,12 @@
+class RaceCar:
+
+    def __init__(self, color, fuel_remaining, **kwargs):
+        self.laps = 0
+        self.color = color
+        self.fuel_remaining = fuel_remaining
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def run_lap(self, length):
+        self.fuel_remaining -= length * 0.125
+        self.laps += 1
--- treehouse/test.py
+++ treehouse/test.py
@@ -1,18 +1,14 @@
-def add(*args):
-    total = 0
-    for num in args:
-        total += num
-    print(total)
+def add(*args): total = 0 for num in args: total += num print(total)
 add(1,2,3,4,5)
 
-
-def packer(**kwargs):
-    print(kwargs)
+def packer(**kwargs): print(kwargs)
 
 packer(name = "kenneth", num = 42, spanish_inquisition = None)
 
-
-def unpacker(first_name, last_name):
-    print("Hi, you are {} {}, right?".format(first_name, last_name))
+def unpacker(first_name, last_name): print("Hi, you are {} {},
+right?".format(first_name, last_name))
 
 unpacker(**{"last_name": "Love", "first_name": "Kenneth"})
+
+#
+#dsafassssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
