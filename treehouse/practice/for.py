dd6b25cb8c776d31f3c43e50c430e720987174ec
Yang Hu <hyytez@gmail.com>
2018-08-21 22:04:33 +0800

    update 2018.8.21

---
+++ treehouse/practice/for.py
@@ -0,0 +1,52 @@
+# Columns: Name, Day/Month, Celebrates, Age
+BIRTHDAYS = (
+    ("James", "9/8", True, 9),
+    ("Shawna", "12/6", True, 22),
+    ("Amaya", "28/2", False, 8),
+    ("Kamal", "29/4", True, 19),
+    ("Sam", "16/7", False, 22),
+    ("Xan", "14/3", False, 34),
+)
+
+# Problem 1: Celebrations
+# Loop through all of the people in BIRTHDAYS
+# If they celebrate their birthday, print out
+# "Happy Birthday" and their name
+print("Task 1 - Celebration:")
+for person in BIRTHDAYS:
+    if True in person:
+        print("Happy Birthday! " + person[0] + "!")
+
+# Problem 2: Half birthdays
+# Loop through all of the people in BIRTHDAYS
+# Calculate their half birthday (six months later)
+# Print out their name and half birthday
+print("Task 2 - Half Birthday:")
+for person in BIRTHDAYS:
+    birth_date = person[1].split("/")
+    birth_day = int(birth_date.pop())
+    output = []
+    if birth_day > 6:
+        birth_day -= 6
+        birth_date.append(str(birth_day))
+    else:
+        birth_day += 6
+        birth_date.append(str(birth_day))
+    birth_date = '/'.join(birth_date)
+    output.append(person[0])
+    output.append(birth_date)
+    print(output)
+
+
+
+# Problem 3: Only school year birthdays
+# Loop through the people in BIRTHDAYS
+# If their birthday is between September (9)
+# And June (6), print their name
+
+
+# Problem 4: Wishing stars
+# Loop through BIRTHDAYS
+# If the person celebrates their birthday,
+# AND their age is 10 or less,
+# Print out their name and as many stars as their age
